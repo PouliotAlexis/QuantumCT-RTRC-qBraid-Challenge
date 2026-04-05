39ef017263abf87bdf11b3aea03f93956d311dc3
Jasmin <peleltierjasmin7@gmail.com>
2026-04-04 21:16:55 -0400

    3 and 4 working

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from utils.CVRPDataLoader import CVRPDataLoader
 from utils.get_cluster import get_cluster_with_optimised_sweep
 from utils.get_distance_matrix import get_distance_matrix_with_ids, remap_route_indices
 from utils.save_crp_solutions import save_cvrp_solution
-from utils.save_instance_results import save_instance_results
+from utils.save_instance_results import get_total_distance, save_instance_results
+from utils.visualise import plot_instance, visualize_solution
 from utils.vqe import solve_tsp
 
 
@@ -21,6 +22,7 @@ def main(instance_id: int) -> None:
     """
     loader = CVRPDataLoader()
     data_instance: dict = loader.get_instance(instance_id)
+    plot_instance(data_instance)
 
     nb_total_gate = 0
     max_nb_qubits = 0
@@ -42,6 +44,9 @@ def main(instance_id: int) -> None:
     # Mettre le 0 en premier en slicant
     all_routes = [r[r.index(0) :] + r[: r.index(0)] + [0] for r in all_routes]
 
+    # Calculate total Euclidean distance
+    total_distance = get_total_distance(all_routes, data_instance["nodes"])
+
     # Save results in the official format
     save_cvrp_solution(
         instance_id,
@@ -49,16 +54,20 @@ def main(instance_id: int) -> None:
         data_dir="data",
     )
 
-    # Save overall data
+    # Save overall data with total distance
     save_instance_results(
-        instance_id, max_nb_qubits, nb_total_gate, time() - start_time, "data/data.csv"
+        instance_id,
+        max_nb_qubits,
+        nb_total_gate,
+        time() - start_time,
+        "data/data.csv",
+        total_distance,
     )
 
+    visualize_solution(data_instance, all_routes)
     print(all_routes)
 
 
 if __name__ == "__main__":
     INSTANCE_ID = 4  # Between 1 and 4
-    main(INSTANCE_ID)
-    main(INSTANCE_ID)
     main(INSTANCE_ID)
